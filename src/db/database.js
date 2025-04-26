d4ad93cebbd2c148e5ea7340249f92e088424ce9
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2025-04-27 02:16:41 +0700

    feat: add db rds

--- src/db/database.js
+++ src/db/database.js
@@ -4,6 +4,7 @@ const { DB, USER, PASSWORD, HOST, DIALECT } = require("../../config/config");
 const database = new Sequelize(DB, USER, PASSWORD, {
   host: HOST,
   dialect: DIALECT,
+  logging: false,
 });
 
 module.exports = database;
